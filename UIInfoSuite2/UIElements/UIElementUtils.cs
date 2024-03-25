43bf2e368f0c3a2d6645770cf21f0d2da72e4dea
Drew Hoener <drewhoener@gmail.com>
2024-03-24 23:38:40 -0400

    fix: Actually hide icons during events

--- UIInfoSuite2/UIElements/UIElementUtils.cs
+++ UIInfoSuite2/UIElements/UIElementUtils.cs
@@ -12,6 +12,8 @@ public static class UIElementUtils
       !Game1.game1.takingMapScreenshot,
       !Game1.eventUp,
       !Game1.viewportFreeze,
+      !Game1.freezeControls,
+      Game1.viewportHold <= 0,
       Game1.displayHUD
     };
 

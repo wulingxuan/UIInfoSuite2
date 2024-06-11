4849cbb7f1038d83f4357cf3a5876690dbd3676e
Drew Hoener <drewhoener@gmail.com>
2024-06-11 16:57:45 -0400

    fix: Use correct logic for getting position above animal when on larger screens
    (cherry picked from commit 0e8a0cb15764c33920cdc8f982a9d5d95d7674af)

--- UIInfoSuite2/UIElements/ShowWhenAnimalNeedsPet.cs
+++ UIInfoSuite2/UIElements/ShowWhenAnimalNeedsPet.cs
@@ -67,9 +67,9 @@ internal class ShowWhenAnimalNeedsPet : IDisposable
 
 
 #region Event subscriptions
-  private void OnWarped(object sender, WarpedEventArgs e) { }
+  private void OnWarped(object? sender, WarpedEventArgs e) { }
 
-  private void OnRenderingHud_DrawNeedsPetTooltip(object sender, RenderingHudEventArgs e)
+  private void OnRenderingHud_DrawNeedsPetTooltip(object? sender, RenderingHudEventArgs e)
   {
     if (UIElementUtils.IsRenderingNormally() &&
         Game1.activeClickableMenu == null &&
@@ -80,7 +80,7 @@ internal class ShowWhenAnimalNeedsPet : IDisposable
     }
   }
 
-  private void OnRenderingHud_DrawAnimalHasProduct(object sender, RenderingHudEventArgs e)
+  private void OnRenderingHud_DrawAnimalHasProduct(object? sender, RenderingHudEventArgs e)
   {
     if (UIElementUtils.IsRenderingNormally() &&
         Game1.activeClickableMenu == null &&
@@ -90,7 +90,7 @@ internal class ShowWhenAnimalNeedsPet : IDisposable
     }
   }
 
-  private void UpdateTicked(object sender, UpdateTickedEventArgs e)
+  private void UpdateTicked(object? sender, UpdateTickedEventArgs e)
   {
     if (!UIElementUtils.IsRenderingNormally() ||
         Game1.activeClickableMenu != null ||
@@ -109,56 +109,60 @@ internal class ShowWhenAnimalNeedsPet : IDisposable
   private void DrawAnimalHasProduct()
   {
     NetLongDictionary<FarmAnimal, NetRef<FarmAnimal>>? animalsInCurrentLocation = GetAnimalsInCurrentLocation();
-    if (animalsInCurrentLocation != null)
+    if (animalsInCurrentLocation == null)
     {
-      foreach (KeyValuePair<long, FarmAnimal> animal in animalsInCurrentLocation.Pairs)
-      {
-        FarmAnimalHarvestType? harvestType = animal.Value.GetHarvestType();
-        if (harvestType != FarmAnimalHarvestType.DropOvernight &&
-            !animal.Value.IsEmoting &&
-            animal.Value.currentProduce.Value != "430" && // 430 is truffle
-            animal.Value.currentProduce.Value != null &&
-            animal.Value.age.Value >= animal.Value.GetAnimalData().DaysToMature)
-        {
-          Vector2 positionAboveAnimal = GetPetPositionAboveAnimal(animal.Value);
-          positionAboveAnimal.Y += (float)(Math.Sin(
-                                             Game1.currentGameTime.TotalGameTime.TotalMilliseconds / 300.0 +
-                                             animal.Value.Name.GetHashCode()
-                                           ) *
-                                           5.0);
-          Game1.spriteBatch.Draw(
-            Game1.emoteSpriteSheet,
-            Utility.ModifyCoordinatesForUIScale(new Vector2(positionAboveAnimal.X + 14f, positionAboveAnimal.Y)),
-            new Rectangle(
-              3 * (Game1.tileSize / 4) % Game1.emoteSpriteSheet.Width,
-              3 * (Game1.tileSize / 4) / Game1.emoteSpriteSheet.Width * (Game1.tileSize / 4),
-              Game1.tileSize / 4,
-              Game1.tileSize / 4
-            ),
-            Color.White * 0.9f,
-            0.0f,
-            Vector2.Zero,
-            4f,
-            SpriteEffects.None,
-            1f
-          );
+      return;
+    }
 
-          string produceItemId = animal.Value.currentProduce.Value;
-          ParsedItemData? produceData = ItemRegistry.GetData(produceItemId);
-          Rectangle sourceRectangle = produceData.GetSourceRect();
-          Game1.spriteBatch.Draw(
-            produceData.GetTexture(),
-            Utility.ModifyCoordinatesForUIScale(new Vector2(positionAboveAnimal.X + 28f, positionAboveAnimal.Y + 8f)),
-            sourceRectangle,
-            Color.White * 0.9f,
-            0.0f,
-            Vector2.Zero,
-            2.2f,
-            SpriteEffects.None,
-            1f
-          );
-        }
+    foreach (KeyValuePair<long, FarmAnimal> animal in animalsInCurrentLocation.Pairs)
+    {
+      FarmAnimalHarvestType? harvestType = animal.Value.GetHarvestType();
+      if (harvestType == FarmAnimalHarvestType.DropOvernight ||
+          animal.Value.IsEmoting ||
+          animal.Value.currentProduce.Value == "430" || // 430 is truffle
+          animal.Value.currentProduce.Value == null ||
+          animal.Value.age.Value < animal.Value.GetAnimalData().DaysToMature)
+      {
+        continue;
       }
+
+      Vector2 positionAboveAnimal = GetPetPositionAboveAnimal(animal.Value);
+      positionAboveAnimal.Y += (float)(Math.Sin(
+                                         Game1.currentGameTime.TotalGameTime.TotalMilliseconds / 300.0 +
+                                         animal.Value.Name.GetHashCode()
+                                       ) *
+                                       5.0);
+      Game1.spriteBatch.Draw(
+        Game1.emoteSpriteSheet,
+        Utility.ModifyCoordinatesForUIScale(new Vector2(positionAboveAnimal.X + 14f, positionAboveAnimal.Y)),
+        new Rectangle(
+          3 * (Game1.tileSize / 4) % Game1.emoteSpriteSheet.Width,
+          3 * (Game1.tileSize / 4) / Game1.emoteSpriteSheet.Width * (Game1.tileSize / 4),
+          Game1.tileSize / 4,
+          Game1.tileSize / 4
+        ),
+        Color.White * 0.9f,
+        0.0f,
+        Vector2.Zero,
+        4f,
+        SpriteEffects.None,
+        1f
+      );
+
+      string produceItemId = animal.Value.currentProduce.Value;
+      ParsedItemData? produceData = ItemRegistry.GetData(produceItemId);
+      Rectangle sourceRectangle = produceData.GetSourceRect();
+      Game1.spriteBatch.Draw(
+        produceData.GetTexture(),
+        Utility.ModifyCoordinatesForUIScale(new Vector2(positionAboveAnimal.X + 28f, positionAboveAnimal.Y + 8f)),
+        sourceRectangle,
+        Color.White * 0.9f,
+        0.0f,
+        Vector2.Zero,
+        2.2f,
+        SpriteEffects.None,
+        1f
+      );
     }
   }
 
@@ -166,41 +170,45 @@ internal class ShowWhenAnimalNeedsPet : IDisposable
   {
     NetLongDictionary<FarmAnimal, NetRef<FarmAnimal>>? animalsInCurrentLocation = GetAnimalsInCurrentLocation();
 
-    if (animalsInCurrentLocation != null)
+    if (animalsInCurrentLocation == null)
     {
-      foreach (KeyValuePair<long, FarmAnimal> animal in animalsInCurrentLocation.Pairs)
+      return;
+    }
+
+    foreach (KeyValuePair<long, FarmAnimal> animal in animalsInCurrentLocation.Pairs)
+    {
+      if (animal.Value.IsEmoting ||
+          animal.Value.wasPet.Value ||
+          (animal.Value.friendshipTowardFarmer.Value >= 1000 && HideOnMaxFriendship))
       {
-        if (!animal.Value.IsEmoting &&
-            !animal.Value.wasPet.Value &&
-            (animal.Value.friendshipTowardFarmer.Value < 1000 || !HideOnMaxFriendship))
-        {
-          Vector2 positionAboveAnimal = GetPetPositionAboveAnimal(animal.Value);
-          string animalType = animal.Value.type.Value.ToLower();
-
-          if (animalType.Contains("cow") ||
-              animalType.Contains("sheep") ||
-              animalType.Contains("goat") ||
-              animalType.Contains("pig"))
-          {
-            positionAboveAnimal.X += 50f;
-            positionAboveAnimal.Y += 50f;
-          }
-
-          Game1.spriteBatch.Draw(
-            Game1.mouseCursors,
-            Utility.ModifyCoordinatesForUIScale(
-              new Vector2(positionAboveAnimal.X, positionAboveAnimal.Y + _yMovementPerDraw.Value)
-            ),
-            new Rectangle(32, 0, 16, 16),
-            Color.White * _alpha.Value,
-            0.0f,
-            Vector2.Zero,
-            4f,
-            SpriteEffects.None,
-            1f
-          );
-        }
+        continue;
       }
+
+      Vector2 positionAboveAnimal = GetPetPositionAboveAnimal(animal.Value);
+      string animalType = animal.Value.type.Value.ToLower();
+
+      if (animalType.Contains("cow") ||
+          animalType.Contains("sheep") ||
+          animalType.Contains("goat") ||
+          animalType.Contains("pig"))
+      {
+        positionAboveAnimal.X += 50f;
+        positionAboveAnimal.Y += 50f;
+      }
+
+      Game1.spriteBatch.Draw(
+        Game1.mouseCursors,
+        Utility.ModifyCoordinatesForUIScale(
+          new Vector2(positionAboveAnimal.X, positionAboveAnimal.Y + _yMovementPerDraw.Value)
+        ),
+        new Rectangle(32, 0, 16, 16),
+        Color.White * _alpha.Value,
+        0.0f,
+        Vector2.Zero,
+        4f,
+        SpriteEffects.None,
+        1f
+      );
     }
   }
 
@@ -208,54 +216,49 @@ internal class ShowWhenAnimalNeedsPet : IDisposable
   {
     foreach (NPC? character in Game1.currentLocation.characters)
     {
-      if (character is Pet pet &&
-          !pet.lastPetDay.Values.Any(day => day == Game1.Date.TotalDays) &&
-          (pet.friendshipTowardFarmer.Value < 1000 || !HideOnMaxFriendship))
+      if (character is not Pet pet ||
+          pet.lastPetDay.Values.Any(day => day == Game1.Date.TotalDays) ||
+          (pet.friendshipTowardFarmer.Value >= 1000 && HideOnMaxFriendship))
       {
-        Vector2 positionAboveAnimal = GetPetPositionAboveAnimal(character);
-        positionAboveAnimal.X += 50f;
-        positionAboveAnimal.Y += 30f;
-        Game1.spriteBatch.Draw(
-          Game1.mouseCursors,
-          Utility.ModifyCoordinatesForUIScale(
-            new Vector2(positionAboveAnimal.X, positionAboveAnimal.Y + _yMovementPerDraw.Value)
-          ),
-          new Rectangle(32, 0, 16, 16),
-          Color.White * _alpha.Value,
-          0.0f,
-          Vector2.Zero,
-          4f,
-          SpriteEffects.None,
-          1f
-        );
+        continue;
       }
+
+      Vector2 positionAboveAnimal = GetPetPositionAboveAnimal(character);
+
+      positionAboveAnimal.X += 50f;
+      positionAboveAnimal.Y += 20f;
+      Game1.spriteBatch.Draw(
+        Game1.mouseCursors,
+        Utility.ModifyCoordinatesForUIScale(
+          new Vector2(positionAboveAnimal.X, positionAboveAnimal.Y + _yMovementPerDraw.Value)
+        ),
+        new Rectangle(32, 0, 16, 16),
+        Color.White * _alpha.Value,
+        0.0f,
+        Vector2.Zero,
+        4f,
+        SpriteEffects.None,
+        1f
+      );
     }
   }
 
   private Vector2 GetPetPositionAboveAnimal(Character animal)
   {
-    return new Vector2(
-      Game1.viewport.Width <= Game1.currentLocation.map.DisplayWidth
-        ? animal.position.X - Game1.viewport.X + 16
-        : animal.position.X + ((Game1.viewport.Width - Game1.currentLocation.map.DisplayWidth) / 2 + 18),
-      Game1.viewport.Height <= Game1.currentLocation.map.DisplayHeight
-        ? animal.position.Y - Game1.viewport.Y - 34
-        : animal.position.Y + ((Game1.viewport.Height - Game1.currentLocation.map.DisplayHeight) / 2 - 50)
-    );
+    Vector2 animalPosition = animal.getLocalPosition(Game1.viewport);
+    animalPosition.X += 10;
+    animalPosition.Y -= 34;
+    return animalPosition;
   }
 
-  private NetLongDictionary<FarmAnimal, NetRef<FarmAnimal>> GetAnimalsInCurrentLocation()
+  private NetLongDictionary<FarmAnimal, NetRef<FarmAnimal>>? GetAnimalsInCurrentLocation()
   {
-    NetLongDictionary<FarmAnimal, NetRef<FarmAnimal>> animals = null;
-
-    if (Game1.currentLocation is AnimalHouse)
+    NetLongDictionary<FarmAnimal, NetRef<FarmAnimal>>? animals = Game1.currentLocation switch
     {
-      animals = (Game1.currentLocation as AnimalHouse).animals;
-    }
-    else if (Game1.currentLocation is Farm)
-    {
-      animals = (Game1.currentLocation as Farm).animals;
-    }
+      AnimalHouse animalHouse => animalHouse.Animals,
+      Farm farm => farm.Animals,
+      _ => null
+    };
 
     return animals;
   }

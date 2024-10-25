b1bdf1756befe90bc582b7c3e19b0cba7e4a4c7d
wulingxuan <1208143667@qq.com>
2024-10-25 04:29:40 -0400

    Translate the "configMenu" string related to IGenericModConfigMenuApi, and transfer the translation to i18n.
    (cherry picked from commit ddcf864ef4b3b7f8742552e6a6fd81f46e0beca0)

--- UIInfoSuite2/UIElements/LocationOfTownsfolk.cs
+++ UIInfoSuite2/UIElements/LocationOfTownsfolk.cs
@@ -270,7 +270,7 @@ internal class LocationOfTownsfolk : IDisposable
 
       if (checkbox.bounds.Contains(Game1.getMouseX(), Game1.getMouseY()))
       {
-        IClickableMenu.drawHoverText(Game1.spriteBatch, "Track on map", Game1.dialogueFont);
+        IClickableMenu.drawHoverText(Game1.spriteBatch, I18n.TrackOnMap(), Game1.dialogueFont);
       }
     }
   }
